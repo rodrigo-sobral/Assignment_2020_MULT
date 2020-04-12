d6aa6c653d395b26b1d981354c0dc805382ca5c0
DreSilva <2000.andre.silva@gmail.com>
2020-04-12 15:56:44 +0100

    Minor changes

--- classes/Personagem.js
+++ classes/Personagem.js
@@ -12,43 +12,43 @@ class Personagem extends Inimigo {
     detect_movement(code) {
         if (this.up==false && (code=="ArrowUp" || code=="KeyW")) {
             this.up=true
-            //if (this.img.id=="yin") this.img.src="../resources/yin_sprite_back.gif"
-            //else this.img.src="../resources/Sasha_sprite_back.gif"
+            //if (this.img.id=="yin") this.img.src="./resources/yin_sprite_back.gif"
+            //else this.img.src="./resources/Sasha_sprite_back.gif"
         } else if (this.down==false && (code=="ArrowDown" || code=="KeyS")) {
             this.down=true
-            //if (this.img.id=="yin") this.img.src="../resources/yin_sprite.gif"
-            //else this.img.src="../resources/Sasha_sprite.gif"
+            //if (this.img.id=="yin") this.img.src="./resources/yin_sprite.gif"
+            //else this.img.src="./resources/Sasha_sprite.gif"
         } else if (this.left==false && (code=="ArrowLeft" || code=="KeyA")) {
             this.left=true
-            //if (this.img.id=="yin") this.img.src="../resources/yin_sprite_left.gif"
-            //else this.img.src="../resources/Sasha_sprite_left.gif"
+            //if (this.img.id=="yin") this.img.src="./resources/yin_sprite_left.gif"
+            //else this.img.src="./resources/Sasha_sprite_left.gif"
         } else if (this.right==false && (code=="ArrowRight" || code=="KeyD")) {
             this.right=true
-            //if (this.img.id=="yin") this.img.src="../resources/yin_sprite_right.gif"
-            //else this.img.src="../resources/Sasha_sprite_right.gif"
+            //if (this.img.id=="yin") this.img.src="./resources/yin_sprite_right.gif"
+            //else this.img.src="./resources/Sasha_sprite_right.gif"
         }
     }
 
     stop(code) {
         if (this.up==true && (code=="ArrowUp" || code=="KeyW")) {
-            //if (this.img.id=="yin") this.img.src="../resources/yin_sprite_back.png"
-            //else this.img.src="../resources/Sasha_sprite_back.png"
+            //if (this.img.id=="yin") this.img.src="./resources/yin_sprite_back.png"
+            //else this.img.src="./resources/Sasha_sprite_back.png"
             this.up=false
         } else if (this.down==true && (code=="ArrowDown" || code=="KeyS")) {
-            //if (this.img.id=="yin") this.img.src="../resources/yin_sprite.png"
-            //else this.img.src="../resources/Sasha_sprite.png"
+            //if (this.img.id=="yin") this.img.src="./resources/yin_sprite.png"
+            //else this.img.src="./resources/Sasha_sprite.png"
             this.down=false
         } else if (this.left==true && (code=="ArrowLeft" || code=="KeyA")) {
-            //if (this.img.id=="yin") this.img.src="../resources/yin_sprite_left.png"
-            //else this.img.src="../resources/Sasha_sprite_left.png"
+            //if (this.img.id=="yin") this.img.src="./resources/yin_sprite_left.png"
+            //else this.img.src="./resources/Sasha_sprite_left.png"
             this.left=false
         } else if (this.right==true && (code=="ArrowRight" || code=="KeyD")) {
-            //if (this.img.id=="yin") this.img.src="../resources/yin_sprite_right.png"
-            //else this.img.src="../resources/Sasha_sprite_right.png"
+            //if (this.img.id=="yin") this.img.src="./resources/yin_sprite_right.png"
+            //else this.img.src="./resources/Sasha_sprite_right.png"
             this.right=false
         }
     }
-    
+
     walking(cw, ch) {
         if (this.left==true) {
             if(this.x>0) this.x-=this.speed
