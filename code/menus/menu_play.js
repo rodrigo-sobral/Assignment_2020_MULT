8798e7bd21037111ade5c76c1afa7392fc59acbc
DreSilva <2000.andre.silva@gmail.com>
2020-05-18 17:15:55 +0100

    Load and Save career is working using local storage Final touches on the load and new game buttons

--- code/menus/menu_play.js
+++ code/menus/menu_play.js
@@ -1,29 +1,42 @@
 "use strict";
 
-(function() {
+(function () {
     window.addEventListener("load", main);
 }());
 
 function main() {
-    var newBtn= document.getElementById("newBtn");
-    var loadBtn= document.getElementById("loadBtn");
-    var backBtn= document.getElementById("backBtn");
+    var newBtn = document.getElementById("newBtn");
+    var loadBtn = document.getElementById("loadBtn");
+    var backBtn = document.getElementById("backBtn");
 
     newBtn.addEventListener("mouseup", newGameMenu)
     loadBtn.addEventListener("mouseup", loadGameEngine)
-    backBtn.addEventListener("mouseup",backMainMenu);
-    
-    newBtn.addEventListener("mouseenter",playSound);
-    loadBtn.addEventListener("mouseenter",playSound);
-    backBtn.addEventListener("mouseenter",playSound);
+    backBtn.addEventListener("mouseup", backMainMenu);
+
+    newBtn.addEventListener("mouseenter", playSound);
+    loadBtn.addEventListener("mouseenter", playSound);
+    backBtn.addEventListener("mouseenter", playSound);
 
 }
 
-function backMainMenu() { location.replace("menu_main.html") }
-function newGameMenu() { location.replace("menu_newgame.html") }
-function loadGameEngine() { location.replace("game.html") }
+function backMainMenu() {
+    location.replace("menu_main.html")
+}
+
+function newGameMenu() {
+    location.replace("menu_newgame.html")
+}
+
+function loadGameEngine() {
+    if (localStorage.getItem('career') === null) {
+        alert("There is no game to be loaded")
+    }
+    else{
+        location.replace("game.html")
+    }
+}
 
 function playSound() {
-    var sound= new Audio("../../resources/sounds/buttonSwitchSound.mp3")
+    var sound = new Audio("../../resources/sounds/buttonSwitchSound.mp3")
     sound.play()
 }
